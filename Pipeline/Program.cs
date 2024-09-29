a242da9a0b579c7826d74a08f04457450997758b
Jesse C <jesse@fracturedcode.net>
2024-09-28 17:59:26 -0700

    add sln, pipeline proj, central package management and props

---
+++ Pipeline/Program.cs
@@ -0,0 +1,19 @@
+﻿using ModularPipelines.Context;
+using ModularPipelines.DotNet.Extensions;
+using ModularPipelines.Host;
+using ModularPipelines.Modules;
+
+await PipelineHostBuilder.Create()
+	.ExecutePipelineAsync();
+
+/*
+ * steps:
+ * start infisical
+ * download infisical API spec
+ * stop infisical
+ * nswag generation
+ * nswag correction
+ * build
+ * pack
+ * upload
+*/
