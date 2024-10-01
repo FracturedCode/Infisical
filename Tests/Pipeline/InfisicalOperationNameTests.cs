392d99166a84633e0cd27c5ee66f24ef77a4f87a
Jesse C <jesse@fracturedcode.net>
2024-09-30 22:04:52 -0700

    start making tests

---
+++ Tests/Pipeline/InfisicalOperationNameTests.cs
@@ -0,0 +1,13 @@
+namespace Net.FracturedCode.Infisical.Tests.Pipeline;
+
+public class InfisicalOperationNameTests
+{
+	[Test]
+	public async Task TestGetOperationName()
+	{
+		// TODO
+		List<(string, string)> inOuts = [
+			("api/v1/dynamic-secrets", "")
+		];
+	}
+}
