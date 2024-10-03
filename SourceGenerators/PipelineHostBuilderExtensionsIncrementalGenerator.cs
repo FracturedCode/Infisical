8e5ee13929568875a5bf2c5d31c79b5c51c5afeb
Jesse C <jesse@fracturedcode.net>
2024-10-03 00:17:30 -0700

    spruce up source generator

--- SourceGenerators/PipelineHostBuilderExtensionsIncrementalGenerator.cs
+++ SourceGenerators/PipelineHostBuilderExtensionsIncrementalGenerator.cs
@@ -14,46 +14,52 @@ public class PipelineHostBuilderExtensionsIncrementalGenerator : IIncrementalGen
 		var moduleTypes = context.SyntaxProvider
 			.CreateSyntaxProvider(isModuleClass, getModuleClass)
 			.Collect();
-
+		
+		
 		context.RegisterSourceOutput(moduleTypes, (c, moduleTypeList) =>
 		{
-			if (moduleTypeList.Length == 0)
-			{
-				return;
-			}
-
 			string code = $$"""
-							using System;
-							using ModularPipelines.Host;
-							using Net.FracturedCode.Infisical.Pipeline.Modules;
-							
-							namespace Net.FracturedCode.Infisical.Pipeline;
-							
-							public static partial class PipelineHostBuilderExtensions
-							{
-								[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Net.FracturedCode.Infisical.SourceGenerators.PipelineHostBuilderExtensionsIncrementalGenerator", "1.0.0")]
-								public static partial PipelineHostBuilder AddAllModules(this PipelineHostBuilder builder)
-								{
-									{{moduleTypeList
-										.Select(m => $"builder.AddModule<{m.Identifier}>();")
-										.Aggregate((x, y) => $"{x}\n{y}")}}
-									return builder;
-								}
-							}
-							""";
+				using System;
+				using ModularPipelines.Host;
+				using Net.FracturedCode.Infisical.Pipeline.Modules;
+				
+				namespace Net.FracturedCode.Infisical.Pipeline;
+				
+				public static partial class PipelineHostBuilderExtensions
+				{
+					[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Net.FracturedCode.Infisical.SourceGenerators.PipelineHostBuilderExtensionsIncrementalGenerator", "1.0.0")]
+					public static partial PipelineHostBuilder AddAllModules(this PipelineHostBuilder builder)
+					{
+						{{moduleTypeList
+							.Select(m => $"builder.AddModule<{m.ClassName}>();")
+							.Aggregate((x, y) => $"{x}\n		{y}")}}
+						return builder;
+					}
+				}
+				""";
 
 			c.AddSource("ModuleLoader.g.cs", SourceText.From(code, Encoding.UTF8));
 		});
 	}
 
+	private record Model
+	{
+		public Model(string className)
+		{
+			ClassName = className;
+		}
+		public string ClassName { get; }
+	}
+
 	private static bool isModuleClass(SyntaxNode node, CancellationToken _)
 	{
 		return node is ClassDeclarationSyntax classDecl &&
-			classDecl.BaseList?.Types.Any(t => t.ToString().Contains("Module")) == true; // TODO be more specific
+			classDecl.BaseList?.Types.Any(t => t.ToString().Contains("Module")) == true;
 	}
 
-	private static ClassDeclarationSyntax getModuleClass(GeneratorSyntaxContext context, CancellationToken _)
+	private static Model getModuleClass(GeneratorSyntaxContext context, CancellationToken _)
 	{
-		return (ClassDeclarationSyntax)context.Node;
+		var cds = (ClassDeclarationSyntax)context.Node;
+		return new Model(cds.Identifier.ValueText);
 	}
 }
