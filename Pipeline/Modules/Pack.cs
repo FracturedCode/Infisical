9fabb5560b774e1b3d5c196058c8e39052253dd2
Jesse C <jesse@fracturedcode.net>
2024-10-04 18:02:47 -0700

    add a prerelease to the package version to allow testing without having to change the package version

--- Pipeline/Modules/Pack.cs
+++ Pipeline/Modules/Pack.cs
@@ -1,4 +1,5 @@
 using System.Text.RegularExpressions;
+using Microsoft.Extensions.Options;
 using ModularPipelines.Attributes;
 using ModularPipelines.Context;
 using ModularPipelines.DotNet.Extensions;
@@ -11,23 +12,23 @@ using File = ModularPipelines.FileSystem.File;
 namespace Net.FracturedCode.Infisical.Pipeline.Modules;
 
 [DependsOn<Build>]
-public partial class Pack : Module<File>
+public partial class Pack(IOptions<NugetOptions> nugetOptions) : Module<File>
 {
 	protected override async Task<File?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
 	{
 		CommandResult packResult = await context.DotNet().Pack(new DotNetPackOptions
-			{
-				ProjectSolution = context.Git().RootDirectory.GetFiles(f => f.Name == "Infisical.csproj").Single(),
-				NoBuild = true,
-				Configuration = "Release"
-			},
-			cancellationToken);
-		var pkgs = packageRegex().Matches(packResult.StandardOutput)
+		{
+			ProjectSolution = context.Git().RootDirectory.GetFiles(f => f.Name == "Infisical.csproj").Single(),
+			NoBuild = true,
+			Configuration = "Release",
+			VersionSuffix = nugetOptions.Value.PushToProd ? null : $"alpha.{context.Git().Information.LastCommitShortSha}"
+		}, cancellationToken);
+		var packages = packageRegex().Matches(packResult.StandardOutput)
 			.Select(f => new File(f.Value))
 			.ToList();
 		// Ensure snupkg exists
-		_ = pkgs.Single(p => p.Extension == ".snupkg");
-		return pkgs.Single(p => p.Extension == ".nupkg");
+		_ = packages.Single(p => p.Extension == ".snupkg");
+		return packages.Single(p => p.Extension == ".nupkg");
 	}
 
 	[GeneratedRegex(@"(?<=Successfully created package ').*(?='\.)")]
